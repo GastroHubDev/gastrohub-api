0eaf94894c9e09279a3fe2ee84f7ec8e824adfaa
Victor <victormazzola2019@gmail.com>
2026-06-13 15:34:42 -0300

    chore: scaffold user type module classes

---
+++ src/main/java/br/com/fiap/gastrohubapi/application/gateway/UserTypeGateway.java
@@ -0,0 +1,4 @@
+package br.com.fiap.gastrohubapi.application.gateway;
+
+public class UserTypeGateway {
+}
---
+++ src/main/java/br/com/fiap/gastrohubapi/application/usecase/usertype/ListUserTypesUseCase.java
@@ -0,0 +1,4 @@
+package br.com.fiap.gastrohubapi.application.usecase.usertype;
+
+public class ListUserTypesUseCase {
+}
