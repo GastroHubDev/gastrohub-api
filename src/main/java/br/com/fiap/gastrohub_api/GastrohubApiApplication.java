fda65fb69c56cf156c01ac6be46bd1351e97a1d8
Victor <victormazzola2019@gmail.com>
2026-06-13 14:35:16 -0300

    initialize gastrohub-api project with Spring Boot and Maven setup

---
+++ src/main/java/br/com/fiap/gastrohub_api/GastrohubApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gastrohub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastrohubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastrohubApiApplication.class, args);
+	}
+
+}
