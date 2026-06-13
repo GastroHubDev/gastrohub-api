a131437d57d438a11b1f84da5abcb4a69001344f
Victor <victormazzola2019@gmail.com>
2026-06-13 14:45:31 -0300

    refactor package structure for Gastrohub API

--- src/main/java/br/com/fiap/gastrohubapi/GastrohubApiApplication.java
+++ src/main/java/br/com/fiap/gastrohubapi/GastrohubApiApplication.java
@@ -1,4 +1,4 @@
-package br.com.fiap.gastrohub_api;
+package br.com.fiap.gastrohubapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/fiap/gastrohubapi/GastrohubApiApplicationTests.java
+++ src/test/java/br/com/fiap/gastrohubapi/GastrohubApiApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.fiap.gastrohub_api;
+package br.com.fiap.gastrohubapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
