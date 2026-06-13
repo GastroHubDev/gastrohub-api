84bf95761b87823b52415b2341f90a98e7075fdd
Victor <victormazzola2019@gmail.com>
2026-06-13 15:52:43 -0300

    feat: add user type gateway contract

--- src/main/java/br/com/fiap/gastrohubapi/application/gateway/UserTypeGateway.java
+++ src/main/java/br/com/fiap/gastrohubapi/application/gateway/UserTypeGateway.java
@@ -1,4 +1,22 @@
 package br.com.fiap.gastrohubapi.application.gateway;
 
-public class UserTypeGateway {
+import br.com.fiap.gastrohubapi.domain.entity.UserType;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserTypeGateway {
+    UserType save(UserType userType);
+
+    Optional<UserType> findById(Long id);
+
+    List<UserType> findAll();
+
+    boolean existsByName(String name);
+
+    boolean existsByNameAndIdNot(String name, Long id);
+
+    boolean isInUse(Long id);
+
+    void deleteById(Long id);
 }
