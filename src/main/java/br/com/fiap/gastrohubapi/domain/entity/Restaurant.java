d20c2904c466cfe49071f06873b757bd9ad3543d
Rafael Teles Espindola <rafatespindola@gmail.com>
2026-06-18 19:38:44 -0300

    fix: Ajustado entidade Restaurant

--- src/main/java/br/com/fiap/gastrohubapi/domain/entity/Restaurant.java
+++ src/main/java/br/com/fiap/gastrohubapi/domain/entity/Restaurant.java
@@ -12,10 +12,12 @@ public class Restaurant {
     private String address;
     private KitchenType kitchenType;
     private String openingHours;
-    private User restaurantOwner;
+    private UUID restaurantOwnerId;
 
 
-    public static Restaurant create(UUID id, String name, String address, KitchenType kitchenType, String openingHours, User restaurantOwner) {
+    private Restaurant() {}
+
+    public static Restaurant create(UUID id, String name, String address, KitchenType kitchenType, String openingHours, UUID restaurantOwnerId) {
 
         Restaurant restaurant = new Restaurant();
         restaurant.setId(id);
@@ -23,91 +25,86 @@ public class Restaurant {
         restaurant.setAddress(address);
         restaurant.setKitchenType(kitchenType);
         restaurant.setOpeningHours(openingHours);
-        restaurant.setRestaurantOwner(restaurantOwner);
+        restaurant.setRestaurantOwnerId(restaurantOwnerId);
 
         return restaurant;
     }
 
-    public static Restaurant create(String name, String address, KitchenType kitchenType, String openingHours, User restaurantOwner) {
+    public static Restaurant create(String name, String address, KitchenType kitchenType, String openingHours, UUID restaurantOwnerId) {
 
         Restaurant restaurant = new Restaurant();
         restaurant.setName(name);
         restaurant.setAddress(address);
         restaurant.setKitchenType(kitchenType);
         restaurant.setOpeningHours(openingHours);
-        restaurant.setRestaurantOwner(restaurantOwner);
+        restaurant.setRestaurantOwnerId(restaurantOwnerId);
 
         return restaurant;
     }
 
-    public static void validateRestaurantOwner(User restaurantOwner){
-        if(restaurantOwner == null) {
-            throw new IllegalArgumentException("OpeningHours cannot be null or empty");
+    private static void validateRestaurantOwnerId(UUID restaurantOwnerId){
+        if(restaurantOwnerId == null) {
+            throw new IllegalArgumentException("RestaurantOwnerId cannot be null");
         }
-
-        // TODO
-        // Adicionar verificação para não permitir usuários do tipo CLIENT serem donos de restaurante.
-        // No momento estou sem o user implementado e não consigo obter seu tipo (OWNER ou CLIENT).
-        // Use: UserTypeNotAllowedForRestaurantOwner
     }
 
-    public static void validateId(UUID id){
+    private static void validateId(UUID id){
         if(id == null) {
             throw new IllegalArgumentException("Id cannot be null");
         }
     }
 
-    public static void validateName(String name){
+    private static void validateName(String name){
         if(name == null || name.trim().isEmpty()) {
             throw new IllegalArgumentException("Name cannot be null or empty");
         }
     }
 
-    public static void validateAddress(String address){
+    private static void validateAddress(String address){
         if(address == null || address.trim().isEmpty()) {
             throw new IllegalArgumentException("address cannot be null or empty");
         }
     }
 
-    public static void validateKitchenType(KitchenType kitchenType){
+    private static void validateKitchenType(KitchenType kitchenType){
         if(kitchenType == null) {
             throw new IllegalArgumentException("KitchenType cannot be null");
         }
     }
 
-    public static void validateOpeningHours(String openingHours){
+    private static void validateOpeningHours(String openingHours){
         if(openingHours == null || openingHours.trim().isEmpty()) {
             throw new IllegalArgumentException("OpeningHours cannot be null or empty");
         }
     }
 
-    public void setId(UUID id) {
+    private void setId(UUID id) {
         validateId(id);
         this.id = id;
     }
 
-    public void setName(String name) {
+    private void setName(String name) {
         validateName(name);
         this.name = name;
     }
 
-    public void setAddress(String address) {
+    private void setAddress(String address) {
         validateAddress(address);
         this.address = address;
     }
 
-    public void setKitchenType(KitchenType kitchenType) {
+    private void setKitchenType(KitchenType kitchenType) {
         validateKitchenType(kitchenType);
         this.kitchenType = kitchenType;
     }
 
-    public void setOpeningHours(String openingHours) {
+    private void setOpeningHours(String openingHours) {
         validateOpeningHours(openingHours);
         this.openingHours = openingHours;
     }
 
-    public void setRestaurantOwner(User restaurantOwner) {
-        validateRestaurantOwner(restaurantOwner);
-        this.restaurantOwner = restaurantOwner;
+    private void setRestaurantOwnerId(UUID restaurantOwnerId) {
+        validateRestaurantOwnerId(restaurantOwnerId);
+        this.restaurantOwnerId = restaurantOwnerId;
     }
 }
