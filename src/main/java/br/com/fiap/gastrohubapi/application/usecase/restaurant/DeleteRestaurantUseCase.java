4270d9ef419c0c1975335757eede00d97d6b0a4d
Rafael Teles Espindola <rafatespindola@gmail.com>
2026-06-19 19:43:24 -0300

    feature: useCases feitos

---
+++ src/main/java/br/com/fiap/gastrohubapi/application/usecase/restaurant/DeleteRestaurantUseCase.java
@@ -0,0 +1,4 @@
+package br.com.fiap.gastrohubapi.application.usecase.restaurant;
+
+public class DeleteRestaurantUseCase {
+}
---
+++ src/main/java/br/com/fiap/gastrohubapi/application/usecase/restaurant/input/UpdateRestaurantInput.java
@@ -0,0 +1,4 @@
+package br.com.fiap.gastrohubapi.application.usecase.restaurant.input;
+
+public record UpdateRestaurantInput() {
+}
